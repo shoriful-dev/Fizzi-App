43445a7ec0bb4e21578144628029573f1be16f9c
MD Shoriful Islam <cmshohelrana50@gmail.com>
2026-08-11 22:55:35 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { repositoryName } from "@/prismicio";
 import "./app.css";
 // import Header from "@/components/Header";
 // import ViewCanvas from "@/components/ViewCanvas";
-// import Footer from "@/components/Footer";
 
 const alpino = localFont({
   src: "../../public/fonts/Alpino-Variable.woff2",
